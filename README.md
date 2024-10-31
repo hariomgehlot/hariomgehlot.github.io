78917003677ead94e8c3560cbe70e30d449856f9
hariom gehlot <hariom.gehlot@artivatic.ai>
2024-10-31 15:38:28 +0530

    new folio

---
+++ README.md
@@ -0,0 +1,17 @@
+<p align="center">
+  <a href="https://github.com/hariomgehlot">
+    <img src="https://github-readme-stats.vercel.app/api?username=hariomgehlot&show_icons=true&theme=radical" />
+  </a>
+  <a href="https://github.com/hariomgehlot?tab=repositories&type=source">
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=hariomgehlot&theme=radical" />
+  </a>
+  <a href="https://github.com/hariomgehlot/hariomgehlot">
+    <img src="https://github-readme-stats.vercel.app/api/pin/?username=hariomgehlot&repo=hariomgehlot&theme=radical" />
+  </a>
+</p>
+
+<p align="center">
+  <a href="https://github.com/hariomgehlot">
+    <img src="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=hariomgehlot&theme=radical" />
+  </a>
+</p>
---
+++ src/data/experience.ts
@@ -0,0 +1,63 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "November 2024 — Present",
+    title: "Software Engineer",
+    company: "Artivatic.ai",
+    companyUrl: "https://www.artivatic.ai",
+    description:
+      "Designed and architected Artivatic.ai's multiple projects, utilizing technologies such as Angular, RxJS, Single-Spa, TypeScript, WebSocket, Git, Express.js, and HTML. Worked on integrating with the Stripe subscription system and collaborated closely with designers to implement pixel-perfect layouts and user flows using Angular CLI.",
+    technologies: [
+      "Angular",
+      "Tailwind CSS",
+      "RxJS",
+      "Single-Spa",
+      "TypeScript",
+      "WebSocket",
+      "Git",
+      "Express.js",
+      "HTML",
+    ],
+    logo: "/logos/av.png",
+  },
+  {
+    dates: "March 2022 — November 2024",
+    title: "ASE",
+    company: "Rakuten Symphony",
+    companyUrl: "https://symphony.rakuten.com",
+    description:
+      "Designed and architected Artivatic.ai's multiple projects, utilizing technologies such as Angular, RxJS, Single-Spa, TypeScript, WebSocket, Git, Express.js, HTML, Angular Material, and Themes. Worked on integrating with the Stripe subscription system and collaborated closely with designers to implement pixel-perfect layouts and user flows using Angular CLI.",
+    technologies: [
+      "Angular",
+      "RxJS",
+      "Single-Spa",
+      "TypeScript",
+      "WebSocket",
+      "Git",
+      "Express.js",
+      "HTML",
+      "Angular Material",
+      "Angular Themes",
+    ],
+    logo: "/logos/rakuten.png",
+  },
+  {
+    dates: "December 2021 — March 2022",
+    title: "Java Full Stack Developer Intern",
+    company: "Virtusa",
+    companyUrl: "https://www.virtusa.com",
+    description:
+      "Worked as a full stack developer using latest Java web technologies such as Spring Boot, Spring Data JPA, Spring Security, RESTful APIs, and Angular. Designed and developed multiple features such as user authentication, authorization, and customer management. Worked on integrating with the Stripe subscription system and collaborated closely with designers to implement pixel-perfect layouts and user flows using Angular CLI.",
+    technologies: [
+      "Spring Boot",
+      "Spring Data JPA",
+      "Spring Security",
+      "RESTful APIs",
+      "Java",
+      "Git",
+      "HTML",
+    ],
+    logo: "/logos/virtusa.jpg",
+  },
+];
